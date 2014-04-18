92281c9d634f222410d41786c1680ee49e514d1a
Brook Mesfin <Brook.Mesfin@vsp.com>
2014-04-17 19:07:14 -0700

    project completed

--- src/complexNumbers/ComplexNumbers.java
+++ src/complexNumbers/ComplexNumbers.java
@@ -1,14 +1,24 @@
 package complexNumbers;
 
-/**
- *
- * @author W0985353
- */
 public class ComplexNumbers {
 
- 
     public static void main(String[] args) {
         
+    	//instantiating ComplexTest with values
+    	ComplexTest a = new ComplexTest(9.5,7.7);
+    	ComplexTest b = new ComplexTest(1.2,3.1);
+    	
+    	//printing to console about program
+    	System.out.println("A complex number in the \nform (x, y) is equal to \nx + yi, where i is \nsquare root of -1.\n");
+    	
+    	//outputting calculations
+    	System.out.println("*-Complex numbers calculations-*");
+    	System.out.println("a = "+a);
+    	System.out.println("b = "+b);
+    	System.out.printf("a + b = %s\n", a.ComplexAddition(b));
+    	System.out.printf("a - b = %s\n", a.ComplexSubtract(b));
+    	System.out.printf("a * b = %s\n", a.ComplexMultiply(b));
+    	System.out.printf("a / b = %s\n", a.ComplexDivide(b));	
     }
     
 }
--- src/complexNumbers/ComplexTest.java
+++ src/complexNumbers/ComplexTest.java
@@ -2,46 +2,44 @@ package complexNumbers;
 
 public class ComplexTest {
     
-    private Double real;
-    private Double imaginary;
+    private Double real; // real part
+    private Double imaginary; // imaginary part
     
     public ComplexTest()
-    {
+    {// constructor that intializes to zero
            this ( 0.0, 0.0 );
     }
 
     public ComplexTest (double r, double i) 
-    {
+    {//Complex function that assigns two private data members
       real = r;
       imaginary = i;
     }
    
-    public ComplexTest ComplexAddition (double r, double i) 
-    {
-      
+    public ComplexTest ComplexAddition (ComplexTest complex ) 
+    {// divides adds numbers
+		return new ComplexTest(real + complex.real, imaginary + complex.imaginary);
     }
     
-    public ComplexTest ComplexSubtract (double r, double i) 
-    {
-      
+    public ComplexTest ComplexSubtract (ComplexTest complex ) 
+    {// divides subtracts numbers
+		return new ComplexTest(real - complex.real, imaginary - complex.imaginary);
     }
     
-    public ComplexTest ComplexMultiply (double r, double i) 
-    {
-      
+    public ComplexTest ComplexMultiply (ComplexTest complex ) 
+    {// divides multiples numbers
+    	return new ComplexTest(real * complex.real - imaginary * complex.imaginary,
+    			real*complex.imaginary + imaginary * complex.real);
     }
     
-    public ComplexTest ComplexDivide (double r, double i) 
-    {
-      
+    public ComplexTest ComplexDivide (ComplexTest complex ) 
+    { // divides complex numbers
+    	return new ComplexTest((real * complex.real + imaginary * complex.imaginary)/(complex.real * complex.real + complex.imaginary * complex.imaginary) ,
+    			(imaginary * complex.real - real * complex.imaginary)/(complex.real * complex.real + complex.imaginary * complex.imaginary));
     }
     
-public ComplexTest complexadd (ComplexTest c) {
-
-double r, i;
-ComplexTest result;
-r=this.real+c.real;
-i=this.imaginary-c.imaginary;
-result = new ComplexTest (r,i);
-return result;}
+    public String toString()
+    { //turn ComplexTest to String
+		return String.format("(%.1f,  %.1f)", real, imaginary);
+	}
 }
